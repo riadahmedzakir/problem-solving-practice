603c467ead673354f448a3be4be70f363df0df50
Riad Ahmed Zakir <33862995+riadahmedzakir@users.noreply.github.com>
2022-08-23 03:38:19 +0600

    Time: 132 ms (38.93%), Space: 53.2 MB (5.04%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function (nums, k) {
+    if (k == nums.length) { return nums; }
+
+    let repeatationMap = {};
+    let freq = [];
+    let result = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        if (repeatationMap[nums[i]]) { repeatationMap[nums[i]] = repeatationMap[nums[i]] + 1; }
+        else { repeatationMap[nums[i]] = 1; }
+
+        freq.push([]);
+    }
+
+    for (let i = 0; i <= nums.length; i++) {
+        freq.push([]);
+    }
+
+    Object.keys(repeatationMap).forEach(key => {
+        freq[repeatationMap[key]].push(parseInt(key));
+    });
+
+
+    for (let i = freq.length - 1; i >= 0; i--) {
+        if (result.length != k && freq[i].length) {
+            result.push(...freq[i]);
+        }
+    }
+    return result;
+};
