be117d1267aa8359dbbf7675769d5744f7c9791b
Riad Ahmed Zakir <33862995+riadahmedzakir@users.noreply.github.com>
2022-09-22 00:48:04 +0600

    Time: 126 ms (28.33%), Space: 45.7 MB (6.22%) - LeetHub

---
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function(nums) {
+    if (!nums || !nums.length) { return null; }
+    let mid = parseInt(nums.length / 2);
+    const node = new TreeNode(nums[mid]);
+    node.left = sortedArrayToBST(nums.slice(0, mid));
+    node.right = sortedArrayToBST(nums.slice(mid + 1, nums.length))
+    return node;     
+};
